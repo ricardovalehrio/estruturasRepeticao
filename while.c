3d2aef31358e0395cc56b9e949aee663e1e08e1c
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2025-10-02 18:48:34 -0300

    first commit

---
+++ while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int i = 1; // variável de incremento
+   
+    while (i <= 5) { // condição de parada
+        printf("%d\n", i); // imprime o valor atual de i - Saida
+        i++; // incrementa 1 em i até ele chegar a 5
+    }
+   return 0;
+}
