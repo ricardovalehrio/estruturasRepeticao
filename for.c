19948ff0ad81543287abb3f7bc942e58b59c5eaa
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2025-10-02 20:52:19 -0300

    For

---
+++ for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main() {
+    for (int i = 1; i <= 5; i++) { // inicialização; condição; incremento; a variável i é declarada dentro do for
+        printf("%d\n", i);
+    }
+   
+    return 0;
+}
